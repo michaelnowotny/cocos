282d46d287dc7de48a732e242c0cfd7cfda2da48
Michael Nowotny <nowotnym@gmail.com>
2020-07-04 02:53:17 -0700

    - added unit tests for two-dimensional gaussian kde

--- cocos/tests/test_scientific/test_gaussian_kde.py
+++ cocos/tests/test_scientific/test_gaussian_kde.py
@@ -6,18 +6,32 @@ import cocos.numerics as cn
 from cocos.scientific.kde import gaussian_kde
 
 n = 1000
+grid_size = 101
 
 
-@pytest.fixture()
-def points() -> np.ndarray:
-    return np.random.randn(n)
+points_1d = np.random.randn(n)
+xi_1d = np.linspace(-5, 5, grid_size)
 
 
-@pytest.fixture()
-def xi() -> np.ndarray:
-    return np.linspace(-5, 5, 101)
+def generate_points_2d():
+    points_x = np.random.randn(1, n)
+    points_y = np.random.randn(1, n)
+    return np.vstack((points_x, points_y))
 
 
+def generate_xi_2d():
+    x_grid = np.linspace(-5.0, 5.0, grid_size)
+    y_grid = np.linspace(-5.0, 5.0, grid_size)
+    xy_grid_x, xy_grid_y = np.meshgrid(x_grid, y_grid)
+    return np.hstack((xy_grid_x.reshape((-1, 1)), xy_grid_y.reshape((-1, 1)))).T
+
+
+points_2d = generate_points_2d()
+xi_2d = generate_xi_2d()
+
+
+@pytest.mark.parametrize('points, xi', [(points_1d, xi_1d),
+                                        (points_2d, xi_2d)])
 def test_gaussian_kde_scipy_vs_cocos(points: np.ndarray,
                                      xi: np.ndarray):
     gkde_cocos = gaussian_kde(points)
@@ -30,6 +44,8 @@ def test_gaussian_kde_scipy_vs_cocos(points: np.ndarray,
                        density_estimate_scipy)
 
 
+@pytest.mark.parametrize('points, xi', [(points_1d, xi_1d),
+                                        (points_2d, xi_2d)])
 def test_gaussian_kde_scipy_vs_cocos_gpu(points: np.ndarray,
                                          xi: np.ndarray):
     gkde_cocos = gaussian_kde(cn.array(points.squeeze()),
