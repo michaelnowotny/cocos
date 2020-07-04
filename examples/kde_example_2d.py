5a5585e3d858de7646c69275a792e4b922f2bcce
Michael Nowotny <nowotnym@gmail.com>
2020-07-03 23:05:12 -0700

    - added 2d kde example

---
+++ examples/kde_example_2d.py
@@ -0,0 +1,81 @@
+import cocos.numerics as cn
+from cocos.numerics.data_types import NumericArray
+from cocos.scientific.kde import gaussian_kde
+from contexttimer import Timer
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as ss
+
+
+n = 2000  # number of data points
+grid_size = 200+1  # number of points at which to evaluate the kde
+R = 10  # number of repetitions for performance benchmark
+
+
+if __name__ == '__main__':
+    # generate random sample
+    points_x = np.random.randn(1, n)
+    points_y = np.random.randn(1, n)
+    points_xy = np.vstack((points_x, points_y))
+    print(f'points_xy.shape = {points_xy.shape}')
+
+    # generate grid at which to evaluate the sample
+    x_grid = np.linspace(-5.0, 5.0, grid_size)
+    y_grid = np.linspace(-5.0, 5.0, grid_size)
+    xy_grid_x, xy_grid_y  = np.meshgrid(x_grid, y_grid)
+    xy_grid = np.hstack((xy_grid_x.reshape((-1, 1)), xy_grid_y.reshape((-1, 1)))).T
+    print(f'xy_grid.shape = {xy_grid.shape}')
+
+    # construct and evaluate scipy gaussian kde object
+    gaussian_kde_scipy = ss.kde.gaussian_kde(points_xy)
+    density_estimate_scipy = gaussian_kde_scipy.evaluate(xy_grid)
+    print(f'density_estimate_scipy.shape = {density_estimate_scipy.shape}')
+
+    # construct and evaluate cocos gaussian kde object using gpu evaluation
+    gaussian_kde_cocos = gaussian_kde(cn.array(points_xy), gpu=True)
+    density_estimate_cocos = np.array(gaussian_kde_cocos.evaluate(xy_grid))
+    print(f'density_estimate_cocos.shape = {density_estimate_cocos.shape}')
+
+    # verify that results are numerically close
+    print(f'maximum absolute difference between results gpu using Cocos and cpu using SciPy: '
+          f'{np.max(abs(density_estimate_cocos - density_estimate_scipy))}')
+
+    if np.allclose(density_estimate_cocos, density_estimate_scipy):
+        print('estimates from cocos and scipy are numerically close')
+    else:
+        print('estimates from cocos and scipy deviate by more than the default tolerance')
+
+    fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    ax = fig.gca(projection='3d')
+    ax.plot_wireframe(X=xy_grid_x,
+                      Y=xy_grid_y,
+                      Z=density_estimate_scipy.reshape((grid_size, grid_size)),
+                      color='b',
+                      # alpha=0.8,
+                      label='SciPy')
+
+    ax.plot_wireframe(X=xy_grid_x,
+                      Y=xy_grid_y,
+                      Z=np.array(density_estimate_cocos.reshape((grid_size, grid_size))),
+                      color='r',
+                      # alpha=0.8,
+                      label='Cocos')
+    plt.legend()
+    plt.show()
+
+    # run benchmark comparing cpu performance using SciPy with gpu performance using Cocos
+    with Timer() as scipy_timer:
+        for _ in range(R):
+            gaussian_kde_scipy.evaluate(xy_grid)
+
+    print(f'Time to evaluate gaussian kde on cpu using scipy was {scipy_timer.elapsed / R} seconds')
+
+    with Timer() as cocos_timer:
+        for _ in range(R):
+            gaussian_kde_cocos.evaluate(xy_grid)
+
+    print(f'Time to evaluate gaussian kde on gpu using cocos was {cocos_timer.elapsed / R} seconds')
+
+    print(f'speedup on gpu is {scipy_timer.elapsed/cocos_timer.elapsed}')
