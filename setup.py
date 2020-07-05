b61212322bfd74d224c0871d67c687833338eeab
Michael Nowotny <nowotnym@gmail.com>
2020-07-04 17:49:59 -0700

    - bumped version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/michaelnowotny/cocos'
 EMAIL = 'nowotnym@gmail.com'
 AUTHOR = 'Michael Christoph Nowotny'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.1.14"
+VERSION = "0.1.15"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
